1dceb60b55584d7ee5201937da117339e7b9602a
Katie <katie.vandervoort@gmail.com>
2020-05-20 10:44:41 -0700

    changede top media page to only have top 10 of each type

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -1,5 +1,9 @@
 class HomepagesController < ApplicationController
   def index
-    @works = Work.all
+    @samp_work = Work.all.sample
+    @albums = Work.where(category: "album")[0..10]
+    @movies = Work.where(category: "movie")[0..10]
+    @books = Work.where(category: "book")[0..10]
+
   end
 end
