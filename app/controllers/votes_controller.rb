2237c2b9ec63c55c055cdfda18018389457afa4b
Katie <katie.vandervoort@gmail.com>
2020-05-26 08:49:32 -0700

    changed formatting for form

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,14 +3,14 @@ class VotesController < ApplicationController
   def create
     user = User.find_by(id: session[:user_id])
     work = Work.find_by(id: params[:work_id])
-    vote = Vote.new(user: user, work: work)
+    @vote = Vote.new(user: user, work: work)
         
-    if vote.save
+    if @vote.save
       flash[:success] = "Successfully upvoted!"
       redirect_back(fallback_location: root_path)
       return
     else
-      flash[:error] = "Something happened. Vote not added."
+      flash[:warning] = "A problem occured: Could not upvote."
       redirect_back(fallback_location: root_path)
       return
     end
