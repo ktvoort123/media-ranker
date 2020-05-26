e22d01b66a9773e7f764ac628e5cc5073bbc310f
Katie <katie.vandervoort@gmail.com>
2020-05-25 17:34:25 -0700

    changed top_ten and work with most votes to model methods in the work model and updated the homepages view and controller

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -1,9 +1,6 @@
 class HomepagesController < ApplicationController
   def index
-    @samp_work = Work.all.sort_by{|work| work.num_votes}.reverse[0]
-    @albums = Work.where(category: "album").sort_by{|work| work.num_votes}.reverse[0..10]
-    @movies = Work.where(category: "movie").sort_by{|work| work.num_votes}.reverse[0..10]
-    @books = Work.where(category: "book").sort_by{|work| work.num_votes}.reverse[0..10]
-
+    @works = Work.all
+    @top_work = @works.work_with_most_votes
   end
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -4,6 +4,15 @@ class Work < ApplicationRecord
     return self.votes.count
   end
 
+  def self.work_with_most_votes
+    return self.all.sort_by{|work| work.num_votes}.reverse[0]
+  end
+
+  def self.top_ten(category)
+    return self.where(category: category).sort_by{|work| work.num_votes}.reverse[0...10]
+  end
+
+
   has_many :votes
 
   validates :category, presence: true
