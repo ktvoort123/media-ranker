0636e011edcc5d4583eeee7aff07ca6fd73a4a1a
Katie <katie.vandervoort@gmail.com>
2020-05-25 17:16:14 -0700

    update the view top media page to actually look at the num votes each work has

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -1,9 +1,9 @@
 class HomepagesController < ApplicationController
   def index
-    @samp_work = Work.all.sample
-    @albums = Work.where(category: "album")[0..10]
-    @movies = Work.where(category: "movie")[0..10]
-    @books = Work.where(category: "book")[0..10]
+    @samp_work = Work.all.sort_by{|work| work.num_votes}.reverse[0]
+    @albums = Work.where(category: "album").sort_by{|work| work.num_votes}.reverse[0..10]
+    @movies = Work.where(category: "movie").sort_by{|work| work.num_votes}.reverse[0..10]
+    @books = Work.where(category: "book").sort_by{|work| work.num_votes}.reverse[0..10]
 
   end
 end
